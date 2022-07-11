bb8c80c8245fca541d8a16cb18d2f2fe725d2357
JoannaBusmail <joanna.busmail@gmail.com>
2022-07-11 17:13:11 +0200

    add match function

--- src/js/0_data.js
+++ src/js/0_data.js
@@ -49,4 +49,52 @@ const imageArray = [
     name: 'sloth',
     image: 'assets/images/sloth.png',
   },
+  {
+    name: 'cat',
+    image: 'assets/images/cat.png',
+  },
+  {
+    name: 'city',
+    image: 'assets/images/city.png',
+  },
+  {
+    name: 'coffee',
+    image: 'assets/images/coffee.png',
+  },
+  {
+    name: 'cupcakes',
+    image: 'assets/images/cupcakes.png',
+  },
+  {
+    name: 'ghost',
+    image: 'assets/images/ghost.png',
+  },
+  {
+    name: 'girl',
+    image: 'assets/images/girl.png',
+  },
+  {
+    name: 'hand',
+    image: 'assets/images/hand.png',
+  },
+  {
+    name: 'heart',
+    image: 'assets/images/heart.png',
+  },
+  {
+    name: 'icecream',
+    image: 'assets/images/icecream.png',
+  },
+  {
+    name: 'land',
+    image: 'assets/images/land.png',
+  },
+  {
+    name: 'meal',
+    image: 'assets/images/meal.png',
+  },
+  {
+    name: 'sloth',
+    image: 'assets/images/sloth.png',
+  },
 ];
--- src/js/1_main.js
+++ src/js/1_main.js
@@ -1,7 +1,11 @@
 'use strict';
+// * ******** 1.Creo un unico elemento DIV en el HTML que contendrá mi tablero
 const board = document.querySelector('.js_board');
 
-//Recorre array de forma aleatoria, lo necesito apra que un lado de la cara de las fichas se cambie de forma aleatoria
+let elementChosen = [];
+let elementChosenId = [];
+
+// **********  2. Recorre array de forma aleatoria (0_data.js)
 //sort = ordena valores deun array en strings - segun unicode
 imageArray.sort(() => 0.5 - Math.random());
 
@@ -18,24 +22,84 @@ console.log(imageArray);
   board.innerHTML += itemBoard;
 }*/
 
+/* *********  3. Recorro array segun longitud. son 24 objetos Así no tengo que pintar cadrado por cuadrado sino recorriendo mi array, pinto automaticamente cada cuadrado
+A cada objeto le agego un elemento IMG / y a ese elemento le incluyo dos atributos SRC , ID
+tambien le agrego clase para los estilos
+Este elemento IMG lo tengo que agregar a un elemento del HTML que es BOARD al que he llamado inicialmente
+A cada elementos le agrego un listener CLICK y una funcion para manejar dicho click
+
+*/
 function printBoard() {
   for (let i = 0; i < imageArray.length; i++) {
     const element = document.createElement('img');
     element.setAttribute('src', 'assets/images/blank.jpg');
     element.setAttribute('id', i);
     element.classList.add('imageStyle');
-    element.addEventListener('click', handleClickElement);
     board.appendChild(element);
+    element.addEventListener('click', handleClickElement);
+
     console.log(element);
   }
 }
 printBoard();
 
+/* *********  5. gestiono las coincidencias
+5.a -- guardo en una constante todos los elementos que contienen el elemento IMG
+5.b -- si el dato del array donde he guardado el nombre en posicion 0 y 1 
+      5.b.a -- por cada dato que contiene el elemento IMG  --> cada ID de cada elemento en la posicion 0 y 1 le agrego el atributo con la imagen OK , si COINCIDEN
+      5.b.b -- si no coinciden le agrego la imagen BLANk
+*/
+
+function handleMatch() {
+  const cards = document.querySelectorAll('img');
+  console.log(cards);
+  if (elementChosen[0] === elementChosen[1]) {
+    cards[elementChosenId[0]].setAttribute('src', 'assets/images/ok.png');
+    cards[elementChosenId[1]].setAttribute('src', 'assets/images/ok.png');
+  } else {
+    cards[elementChosenId[0]].setAttribute('src', 'assets/images/blank.jpg');
+    cards[elementChosenId[1]].setAttribute('src', 'assets/images/blank.jpg');
+  }
+
+  elementChosen = [];
+  elementChosenId = [];
+}
+//como yo lo hubiera hecho, pero da ERROR
+
+/*function handleMatch() {
+  if (elementChosen[0] === elementChosen[1]) {
+    elementChosen[0].setAttribute('src', 'assets/images/ok.png');
+    elementChosen[1].setAttribute('src', 'assets/images/ok.png');
+  } else {
+    elementChosen[0].setAttribute('src', 'assets/images/blank.jpg');
+    elementChosen[1].setAttribute('src', 'assets/images/blank.jpg');
+  }
+
+  elementChosen = [];
+  
+}*/
+
+/* ********   4. Funcion que controla el click sobre cada elemento
+4.a -- Identifico que elemento he clicado
+4.b -- guardo en una constante en nombre del elemento y en otro la imagen
+4.c -- A ese elemento clicado le agrego el atributo SRC para guardar su imagen correspondiente
+4.d -- creo dos arrays vacíos para guardar en uno el nombre y en otro el id
+4.e -- Hago un condicional -- si el array de eleentos clicados tiene dos datos --> se aplica la funcion handlMatch a partir de 500ms
+*/
 function handleClickElement(event) {
   const clickedElement = event.currentTarget.id;
   const clickedInfo = imageArray[clickedElement].name;
+  const clickedImg = imageArray[clickedElement].image;
+  const elementUpDown = this.setAttribute('src', clickedImg);
+  elementChosen.push(clickedInfo);
+  elementChosenId.push(clickedElement);
+
   console.log(clickedElement);
   console.log(clickedInfo);
+  console.log(elementChosen);
+  if (elementChosen.length === 2) {
+    setTimeout(handleMatch, 500);
+  }
 }
 
 //otra forma de recoger el id sobre el que estoy clicando
